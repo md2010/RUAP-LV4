23deba1a5ee94081548fe2aa27e3316675a683fb
md2010 <76563833+md2010@users.noreply.github.com>
2021-12-01 11:25:45 +0100

    Update AddToChartForGuests.cs

--- Diplomski/1. godina/RUAP/LV4/Opencart/AddToChartForGuests.cs
+++ Diplomski/1. godina/RUAP/LV4/Opencart/AddToChartForGuests.cs
@@ -43,6 +43,7 @@ namespace SeleniumTests
         public void TheAddToChartForGuestsTest()
         {
             driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.LinkText("Components")).Click();
             driver.FindElement(By.LinkText("Monitors (2)")).Click();
             driver.FindElement(By.XPath("//div[@id='content']/div[3]/div[2]/div/div[2]/div[2]/button/span")).Click();
             driver.FindElement(By.XPath("//div[@id='cart']/button")).Click();
