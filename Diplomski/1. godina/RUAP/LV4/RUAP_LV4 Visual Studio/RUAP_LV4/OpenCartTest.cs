d6bca7c2147bb1f41473c9e1f0cebb9caf387866
md2010 <76563833+md2010@users.noreply.github.com>
2021-12-01 11:24:12 +0100

    Update OpenCartTest.cs

--- Diplomski/1. godina/RUAP/LV4/RUAP_LV4 Visual Studio/RUAP_LV4/OpenCartTest.cs
+++ Diplomski/1. godina/RUAP/LV4/RUAP_LV4 Visual Studio/RUAP_LV4/OpenCartTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -89,6 +89,7 @@ namespace RUAP_LV4
         public void TheAddToChartRequiringFillingAFormTest()
         {
             driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.LinkText("Components")).Click();
             driver.FindElement(By.LinkText("Monitors (2)")).Click();
             driver.FindElement(By.XPath("//div[@id='content']/div[3]/div/div/div[2]/div[2]/button/span")).Click();
             driver.FindElement(By.XPath("//div[@id='product']/div")).Click();
